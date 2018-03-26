94486dc3afa09dd860d8abae5542ed0505c84345
Maria Helena Oliveira <mariaholiveira34@gmail.com>
2018-03-26 12:05:02 -0300

    grafico de rede

---
+++ redes.R
@@ -0,0 +1,41 @@
+dados <- read.delim("fornecedor2.txt", head = TRUE, sep = "\t", stringsAsFactors = FALSE)
+library(d3Network)
+
+rm_accent <- function(str,pattern="all") {
+  if(!is.character(str))
+    str <- as.character(str)
+  pattern <- unique(pattern)
+  if(any(pattern=="Ç"))
+    pattern[pattern=="Ç"] <- "ç"
+  symbols <- c(
+    acute = "áéíóúÁÉÍÓÚýÝ",
+    grave = "àèìòùÀÈÌÒÙ",
+    circunflex = "âêîôûÂÊÎÔÛ",
+    tilde = "ãõÃÕñÑ",
+    umlaut = "äëïöüÄËÏÖÜÿ",
+    cedil = "çÇ"
+  )
+  nudeSymbols <- c(
+    acute = "aeiouAEIOUyY",
+    grave = "aeiouAEIOU",
+    circunflex = "aeiouAEIOU",
+    tilde = "aoAOnN",
+    umlaut = "aeiouAEIOUy",
+    cedil = "cC"
+  )
+  accentTypes <- c("´","`","^","~","¨","ç")
+  if(any(c("all","al","a","todos","t","to","tod","todo")%in%pattern)) # opcao retirar todos
+    return(chartr(paste(symbols, collapse=""), paste(nudeSymbols, collapse=""), str))
+  for(i in which(accentTypes%in%pattern))
+    str <- chartr(symbols[i],nudeSymbols[i], str)
+  return(str)
+}
+
+dados$Alvo <- rm_accent(dados$Alvo) 
+dados$Relacionado..a. <- rm_accent(dados$Relacionado..a.) 
+d3SimpleNetwork(dados[, c(1,2)], width = 1200, linkColour = "skyblue", linkDistance = 100,
+                        height = 1200, fontsize = 16, opacity = 0.9,
+                        file = "ExampleGraph.html", iframe = TRUE, textColour = "black")
+
+
+?d3SimpleNetwork
