2dc05b2bac45711ee7bd371078eb693659320d15
Maria Helena Oliveira <mariaholiveira34@gmail.com>
2018-06-14 14:41:47 -0300

    Add files via upload
    shiny de classificação com resumo

--- app.R
+++ app.R
@@ -1,11 +1,29 @@
 library(shiny)
+options(shiny.maxRequestSize=30*1024^2) 
+
+foradoprazo <- function(y, tabela, input){
+  falsos <- subset(y, y[, ncol(y)] == FALSE)
+  checknova <- subset(tabela, RO==1)
+  x <- data.frame()
+  for (i in c(1:nrow(falsos))){
+    fone <- falsos[, which(names(falsos) == input$telefones2)][i]
+    if(fone %in% checknova[, which(names(checknova) == input$telefones)]){
+      grampos <-checknova[checknova[, which(names(checknova) == input$telefones)]  == fone, ]
+      #grampos <- subset(grampos, RO == 1)
+      d6dia <- grampos$DATAFIM +1
+      
+      if(falsos$DATA[i] %in% d6dia){x <- rbind(x, falsos[i, ])}
+      
+     
+    }}
+   return(x)
+}
 
 classif <- function(chamadas, tabela, input){
   tabela$DATAINICIO <- as.Date.character(tabela[, names(tabela)[which(names(tabela) == input$datainicio)]], "%d/%m/%Y")
   tabela$DATAFIM <- as.Date.character(tabela[, names(tabela)[which(names(tabela) == input$datafim)]] ,"%d/%m/%Y")
   chamadas$DATA <- as.Date.character(chamadas[, names(chamadas)[which(names(chamadas) == input$data)]], "%d/%m/%Y")
   telefones <- unique(chamadas[, which(names(chamadas) == input$telefones2)])
-  print(telefones)
   y <- data.frame()
   for (i in telefones){
     
@@ -20,17 +38,35 @@ classif <- function(chamadas, tabela, input){
       vetor <- c(vetor, seq(as.Date(j), as.Date(subset(check$DATAFIM, check$DATAINICIO == j), "%d/%m/%Y")[1], by = "days"))
     }
     
-    xx <- cbind(lig, as.numeric(lig$DATA) %in% vetor)
-    y <- rbind(y, xx) } 
+    xx <- cbind(lig, ifelse(as.numeric(lig$DATA) %in% vetor==TRUE, "Autorizada", FALSE))
+    y <- rbind(y, xx) 
+  } 
+  x <- foradoprazo(y, tabela, input)
+  y[, ncol(y)] <- ifelse(y[, which(names(y) == input$ID)] %in% x[, which(names(x)==input$ID)], "Fora do Prazo", y[, ncol(y)] )
+  vetor <- c()
+  for (i in y[, ncol(y)]){
+    if( i == 1){
+      vetor <- c(vetor, "Autorizada")
+    }
+    else{if(i == 2){
+      vetor <- c(vetor, "Não Autorizada")
+    }
+    else{ vetor <- c(vetor, "Fora do Prazo")}}
+    
+  }
+  y <- cbind(chamadas, vetor)
+  colnames(y) <-  c(names(chamadas), "classificação")
   return(y)
 }
+
+
 
 
 # Define UI for data upload app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Uploading Files"),
+  titlePanel("Classificação de Interceptação Telefônica"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -39,7 +75,7 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Input: Select a file ----
-      fileInput("file1", "Choose CSV File",
+      fileInput("file1", "Escolha o arquivo com as datas de início e fim dos períodos de interceptação.",
                 
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
@@ -52,27 +88,30 @@ ui <- fluidPage(
       uiOutput("dataFim"), 
      
       
-      fileInput("file2", "Choose CSV File",
+      fileInput("file2", "Escolha o arquivo com os dados das chamadas interceptadas",
                 
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
       uiOutput("Telefones2"),
       
-      
+      uiOutput("id"), 
       uiOutput("data"), 
-      actionButton("check", "Enviar")
+      actionButton("check", "Enviar"), 
+      downloadButton("download", "Download")
       # Input: Select number of rows to display ----
       
       
     ),
     
     # Main panel for displaying outputs ----
-    mainPanel(
+    mainPanel(tabsetPanel(
       
       # Output: Data file ----
-      tableOutput("contents")
+      tabPanel("tabela", tableOutput("contents")),
       
+      tabPanel("Resumo", verbatimTextOutput("resumo"))
+    )
     )
     
   )
@@ -82,12 +121,12 @@ ui <- fluidPage(
 server <- function(input, output) {
   filedata1 <- reactive({
     req(input$file1)
-    read.csv(input$file1$datapath, header = TRUE, sep = ";")
+    read.csv(input$file1$datapath, header = TRUE, sep = ";", stringsAsFactors = FALSE)
   })
   
   filedata2 <- reactive({
     req(input$file2)
-    read.csv(input$file2$datapath, header = TRUE, sep = ";")
+    read.csv(input$file2$datapath, header = TRUE, sep = ";", stringsAsFactors = FALSE)
   })
   
   
@@ -139,21 +178,43 @@ server <- function(input, output) {
     
   })
   
-  observeEvent(input$check, output$contents <-
-                 renderTable({
-                   tabela <- read.csv(input$file1$datapath, header = TRUE, sep = ";")
+  output$id <- renderUI({
+    df2 <-filedata2()
+    if (is.null(df2)) return(NULL)
+    
+    items=names(df2)
+    names(items)=items
+    selectInput("ID", "Coluna de ID", items)
+    
+  })
+  
+  observeEvent(input$check, {
+    tab <- reactive({  tabela <- filedata1() #read.csv(input$file1$datapath, header = TRUE, sep = ";")
+    
+    chamadas <- filedata2() #read.csv(input$file2$datapath, header = TRUE, sep = ";")
+    
+    y <- classif(chamadas, tabela, input)
+    y
+    })
+    
+    
+    output$contents <-
+                 renderTable({ tab()}
                   
-                   chamadas <- read.csv(input$file2$datapath, header = TRUE, sep = ";")
-                  
-           
-                   
-                   y <- classif(chamadas, tabela, input)
-                   y
-                   }
+                   #output$resumo <- renderTable(summary("contents"))
                  )
-  )
+    output$resumo <- renderPrint({
+      tabela1 <- tab()
+      summary(tabela1[, ncol(tabela1)])
+    })
+  
+  output$download <- tabedownloadHandler(
+    filename = paste0("download_", Sys.Date(),".csv"), 
+    content = function(file){
+      write.csv(y, file)
+       }
+     )})
 }
 
 # Create Shiny app ----
 shinyApp(ui, server)
-
